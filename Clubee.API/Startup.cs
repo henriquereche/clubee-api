7c824c9e6e360c69188cbd902fd3232ded273295
Henrique Reche <hreche@leanwork.com.br>
2019-11-01 20:57:52 -0300

    Inclusão documentação autogerada swagger.

--- Clubee.API/Startup.cs
+++ Clubee.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Clubee.API
 {
@@ -33,6 +34,11 @@ namespace Clubee.API
                 config.LowercaseQueryStrings = true;
                 config.LowercaseUrls = true;
             });
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new Info { Title = "Clubee API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +53,13 @@ namespace Clubee.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(config =>
+            {
+                config.SwaggerEndpoint("/swagger/v1/swagger.json", "Clubee API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
