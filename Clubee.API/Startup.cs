c3af16ca2e46a96f89e73da026f7ed432524b463
Henrique Reche <hreche@leanwork.com.br>
2019-11-01 20:54:41 -0300

    Configuração padrão de rotas.

--- Clubee.API/Startup.cs
+++ Clubee.API/Startup.cs
@@ -26,6 +26,13 @@ namespace Clubee.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Routing settings.
+            services.AddRouting(config =>
+            {
+                config.LowercaseQueryStrings = true;
+                config.LowercaseUrls = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
