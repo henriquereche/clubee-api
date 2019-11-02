ee8c08b2080f8a0ef719c4378a06e9c3b23e1ed6
Henrique Reche <hreche@leanwork.com.br>
2019-11-01 21:09:08 -0300

    Adição de logging utilizando o console STDIO

--- Clubee.API/Program.cs
+++ Clubee.API/Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Clubee.API
@@ -19,6 +13,7 @@ namespace Clubee.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(options => options.AddConsole())
                 .UseStartup<Startup>();
     }
 }
