9a662c07cce72191bf267df5034069b19aa42014
Henrique Reche <hreche@leanwork.com.br>
2019-11-01 21:05:02 -0300

    Extensão para tentativa de consulta de valores no IConfiguration.

---
+++ Clubee.API/Contracts/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Clubee.API.Contracts.Extensions
+{
+    public static class IConfigurationExtensions
+    {
+        /// <summary>
+        /// Try get value from configuration source.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="configuration"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool TryGetValue<T>(this IConfiguration configuration, string key, out T value)
+        {
+            value = default;
+
+            try
+            {
+                value = configuration.GetValue<T>(key);
+                return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
