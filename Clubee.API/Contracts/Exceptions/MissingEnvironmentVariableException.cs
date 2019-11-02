36094f75ae7f51ceefaf30398bf18b48e67f2455
Henrique Reche <hreche@leanwork.com.br>
2019-11-01 21:08:03 -0300

    Padrão de exceptions para a falta de variaveis de ambiente na aplicação.

---
+++ Clubee.API/Contracts/Exceptions/MissingEnvironmentVariableException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Clubee.API.Contracts.Exceptions
+{
+    /// <summary>
+    /// Exception format to handle missing environment variables.
+    /// </summary>
+    public class MissingEnvironmentVariableException : Exception
+    {
+        public MissingEnvironmentVariableException(string variableName)
+            : base($"Missing environment variable {variableName}.") { }
+    }
+}
