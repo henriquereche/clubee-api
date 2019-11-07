96361bf1eef08546c4a9b45b0c2905d1312cbaeb
Henrique Reche <hreche@leanwork.com.br>
2019-11-07 18:43:08 -0300

    Model representando um upload de uma imagem ao storage

---
+++ Clubee.API/Models/Base/UploadImageModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Clubee.API.Models.Base
+{
+    public class UploadImageModel
+    {
+        public readonly string ImageUrl;
+        public readonly string ThumbnailUrl;
+
+        public UploadImageModel(string imageUrl, string thumbnailUrl)
+        {
+            this.ImageUrl = imageUrl;
+            this.ThumbnailUrl = thumbnailUrl;
+        }
+    }
+}
